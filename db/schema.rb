8573a264a964aa172b67cd7a577bb329db3bbe40
kmwarren <kmwarren@ncsu.edu>
2016-01-28 21:21:15 -0500

    Move to postgres and use haml for the views

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160126235052) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "job_postings", force: :cascade do |t|
     t.string   "job"
     t.string   "company"
